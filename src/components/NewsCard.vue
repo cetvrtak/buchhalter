b0482da5be6483cf8a5ca071ade0988bc080df2e
cetvrtak <59363714+cetvrtak@users.noreply.github.com>
2023-09-24 09:55:09 +0200

    Move news card style to its component

--- src/components/NewsCard.vue
+++ src/components/NewsCard.vue
@@ -21,3 +21,47 @@ export default {
     </div>
   </div>
 </template>
+
+<style>
+.news-card {
+  border-radius: 32px;
+  background: #fff;
+  box-shadow: 0px 5px 50px 0px rgba(0, 0, 0, 0.05);
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  flex: 1 0 0;
+.news-image {
+  border-radius: 32px 32px 0px 0px;
+  width: 100%;
+}
+.news-text {
+  display: flex;
+  padding: 24px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 24px;
+  align-self: stretch;
+}
+.news-desc {
+  align-self: stretch;
+
+  color: #0a2641;
+  font-family: Inter;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 28px; /* 140% */
+}
+.news-date {
+  align-self: stretch;
+
+  color: #5f83a7;
+  font-family: Inter;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 24px; /* 150% */
+}
+</style>
